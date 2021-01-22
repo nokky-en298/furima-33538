c9b3c42903e9ddb3614547b6fdad23c6abc0c2b0
nokky-en298 <enapple.tkmkt7@gmail.com>
2021-01-22 12:08:07 +0900

    商品出品ページの表示と出品した商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,21 @@ class ItemsController < ApplicationController
 
 
   def index
-    @items = Item.includes(:user)
+    @items = Item.all.order('created_at desc')
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,10 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :name, :text
-    validates :price format: ( with: "/^[0-9]+$/", greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: 'Price Half-width number')
-    validates :image format: 
+    validates :image, :name, :text
+    validates :price, numericality: { with: /^[0-9]+$/, message: "Half-width number / Out of setting range" ,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: 'Select'} do
     validates :category_id
     validates :condition_id
     validates :postage_id
